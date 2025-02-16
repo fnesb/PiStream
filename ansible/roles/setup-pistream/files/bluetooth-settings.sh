a41ced7d6fdb0328292471f22b47a93f44527f0b
Felix Nesbigall <fnesbigall@hs-worms.de>
2025-02-16 20:16:10 +0100

    more logging, working jellyfin client, fixes

--- ansible/roles/setup-pistream/files/bluetooth-settings.sh
+++ ansible/roles/setup-pistream/files/bluetooth-settings.sh
@@ -5,6 +5,7 @@
 # Functions
 
 update_device_list() {
+    log_message "bluetooth-settings.sh - update_device_list(): device list update function called" $LINENO
     while true; do
         DEVICES=$(bluetoothctl devices | awk '{for (i=2; i<=NF; i++) printf $i " "; print ""}')
         echo -e "$DEVICES" > /tmp/bluetooth_devices
@@ -13,79 +14,110 @@ update_device_list() {
 }
 
 cleanup() {
-    kill $update_device_list_pid
+    log_message "bluetooth-settings.sh - cleanup(): Cleanup function called" $LINENO
+    kill $update_device_list_pid || log_error "bluetooth-settings.sh - cleanup(): Failed to kill update_device_list_pid" $LINENO
     rm /tmp/bluetooth_devices
-    bluetoothctl scan off
+    kill $bluetoothscan_on_pid || log_error "bluetooth-settings.sh - cleanup(): Failed to kill bluetoothscan_on_pid" $LINENO
     notify-send "Bluetooth" "Scanning stopped."
+    kill $bluetoothscan_on_pid || log_error "bluetooth-settings.sh - cleanup(): Failed to kill bluetoothscan_on_pid" $LINENO
+    kill $bluetoothscan_off_pid || log_error "bluetooth-settings.sh - cleanup(): Failed to kill bluetoothscan_off_pid" $LINENO
 }
-trap cleanup EXIT 
+
+
+# Trap errors
+
+trap 'log_error "Error on line $LINENO. Exit."; exit 1' ERR
+trap cleanup EXIT SIGINT SIGHUP SIGTERM SIGKILL
 
 # Main
 
 bluetoothctl scan on &
-notify-send "Bluetooth" "Scanning for devices..."
-update_device_list &
+bluetoothscan_pid=$!
+notify-send "Bluetooth" "Scanning enabled."
+log_message "Scanning enabled." $LINENO
+update_device_list & || log_error "Failed to start update_device_list"
 update_device_list_pid=$!
 
-AVAILABLE_DEVICES=$(cat /tmp/bluetooth_devices)
-MSG_AVAILABLE_DEVICES=$(cat /tmp/bluetooth_devices | sed 's/^/<span>/;s/$/<\/span>/')
-MESSAGE="<b>Available Devices:</b><br>$MSG_AVAILABLE_DEVICES"
-CHOICE=$(echo -e "Pair Device\nRemove Device\nList Paired Devices\nBack" | rofi -dmenu -p "Bluetooth" -i -mesg "$MESSAGE")
+log_message "Entering scanning loop" $LINENO
+i=0
+for i in (1..9); do
+    dots=$(printf "%0.s." $(seq 1 $i))
+    rofi -e "Scanning$dots"
+    sleep 1
+done
 
-case "$CHOICE" in
-    "Pair Device")
-        DEVICE_TO_PAIR=$(echo -e "$AVAILABLE_DEVICES" | rofi -dmenu -p "Select Device to Pair" -i)
-        if [ -n "$DEVICE_TO_PAIR" ]; then
-            DEVICE_MAC=$(echo "$DEVICE_TO_PAIR" | awk '{print $1}')
-            
-            bluetoothctl pair "$DEVICE_MAC"
-            i=0
-            while true; do
-                PAIR_STATUS=$(bluetoothctl info "$DEVICE_MAC" | grep "Paired: yes")
-                if [ -n "$PAIR_STATUS" ]; then
-                    notify-send "Bluetooth" "Device paired successfully."
-                    break
-                fi
-                i=$((i+1))
-                dots=$(printf "%0.s." $(seq 1 $i))
-                rofi -e "Pairing$dots"
-                sleep 1
+log_message "Entering main loop" $LINENO
+e=0
+while true; do
+    e=$((e+1))
+    log_message "Main loop iteration $c" $LINENO
+    AVAILABLE_DEVICES=$(cat /tmp/bluetooth_devices)
+    MSG_AVAILABLE_DEVICES=$(cat /tmp/bluetooth_devices | sed 's/^/<span>/;s/$/<\/span>/')
+    MESSAGE="<b>Available Devices:</b>$MSG_AVAILABLE_DEVICES"
+    CHOICE=$(echo -e "Pair Device\nRemove Device\nList Paired Devices\nUpdate List\nBack" | rofi -dmenu -p "Bluetooth" -i -mesg "$MESSAGE")
+
+    case "$CHOICE" in
+        "Pair Device")
+            log_message "Pair Device selected" $LINENO
+            DEVICE_TO_PAIR=$(echo -e "$AVAILABLE_DEVICES" | rofi -dmenu -p "Select Device to Pair" -i)
+            if [ -n "$DEVICE_TO_PAIR" ]; then
+                DEVICE_MAC=$(echo "$DEVICE_TO_PAIR" | awk '{print $1}')
+                log_message "Attempt to pair device $DEVICE_MAC" $LINENO
+                bluetoothctl pair "$DEVICE_MAC" || log_error "Failed to pair device $DEVICE_MAC" $LINENO
+                i=0
+                while true; do
+                    PAIR_STATUS=$(bluetoothctl info "$DEVICE_MAC" | grep "Paired: yes")
+                    if [ -n "$PAIR_STATUS" ]; then
+                        notify-send "Bluetooth" "Device paired successfully."
+                        log_message "Pairing successful" $LINENO
+                        break
+                    fi
+                    i=$((i+1))
+                    dots=$(printf "%0.s." $(seq 1 $i))
+                    rofi -e "Pairing$dots"
+                    sleep 1
+                        if [ $i -eq 30 ]; then
+                            notify-send "Bluetooth" "Pairing failed."
+                            log_message "Pairing failed" $LINENO
+                            return 1
+                        fi
+                done
+                log_message "Attempt to connect to device $DEVICE_MAC" $LINENO
+                bluetoothctl connect "$DEVICE_MAC" || log_error "Failed to connect to device $DEVICE_MAC" $LINENO
+                i=0
+                while true; do
+                    CONNECT_STATUS=$(bluetoothctl info "$DEVICE_MAC" | grep "Connected: yes")
+                    if [ -n "$CONNECT_STATUS" ]; then
+                        notify-send "Bluetooth" "Device connected successfully."
+                        log_message "Connection successful" $LINENO
+                        break
+                    fi
+                    i=$((i+1))
+                    dots=$(printf "%0.s." $(seq 1 $i))
+                    rofi -e "Connecting$dots"
+                    sleep 1
                     if [ $i -eq 30 ]; then
-                        notify-send "Bluetooth" "Pairing failed."
+                        notify-send "Bluetooth" "Connection failed."
+                        log_message "Connection failed" $LINENO
                         return 1
                     fi
-            done
-
-            bluetoothctl connect "$DEVICE_MAC"
-            i=0
-            while true; do
-                CONNECT_STATUS=$(bluetoothctl info "$DEVICE_MAC" | grep "Connected: yes")
-                if [ -n "$CONNECT_STATUS" ]; then
-                    notify-send "Bluetooth" "Device connected successfully."
-                    break
-                fi
-                i=$((i+1))
-                dots=$(printf "%0.s." $(seq 1 $i))
-                rofi -e "Connecting$dots"
-                sleep 1
-                if [ $i -eq 30 ]; then
-                    notify-send "Bluetooth" "Connection failed."
-                    return 1
-                fi
-            done
-        fi
-        ;;
-    "Remove Device")
+                done
+            fi
+            ;;
+        "Remove Device")
+            log_message "Remove Device selected" $LINENO
             PAIRED_DEVICES=$(bluetoothctl paired-devices | awk '{for (i=2; i<=NF; i++) printf $i " "; print ""}')
             DEVICE_TO_REMOVE=$(echo -e "$PAIRED_DEVICES" | rofi -dmenu -p "Select Device to Remove" -i)
             if [ -n "$DEVICE_TO_REMOVE" ]; then
                 DEVICE_MAC=$(echo "$DEVICE_TO_REMOVE" | awk '{print $1}')
-                bluetoothctl remove "$DEVICE_MAC"
-                 i=0
+                log_message "Attempt to remove device $DEVICE_MAC" $LINENO
+                bluetoothctl remove "$DEVICE_MAC" || log_error "Failed to remove device $DEVICE_MAC" $LINENO
+                i=0
                 while true; do
                     REMOVE_STATUS=$(bluetoothctl info "$DEVICE_MAC" | grep "not available")
                     if [ -n "$REMOVE_STATUS" ]; then
                         notify-send "Bluetooth" "Device removed successfully."
+                        log_message "Removal successful" $LINENO
                         break
                     fi
                     i=$((i+1))
@@ -94,18 +126,26 @@ case "$CHOICE" in
                     sleep 1
                     if [ $i -eq 30 ]; then
                         notify-send "Bluetooth" "Removal failed."
+                        log_message "Removal failed" $LINENO
                         return 1
                     fi
                 done
             fi
-        ;;
-    "List Paired Devices")
+            ;;
+        "List Paired Devices")
+            log_message "List Paired Devices selected" $LINENO
             PAIRED_DEVICES=$(bluetoothctl paired-devices | awk '{for (i=2; i<=NF; i++) printf $i " "; print ""}')
             GO_BACK=$(echo -e "Back" | rofi -dmenu -p "Paired Devices" -i -mesg "$PAIRED_DEVICES")
-        ;;
-    "Back")
-        break
-        ;;
-esac
-
+            ;;
+        "Update List")
+            log_message "Update List selected" $LINENO
+            continue
+            ;;
+        "Back")
+            log_message "Back selected" $LINENO
+            cleanup || log_error "Failed to run cleanup" $LINENO
+            break
+            ;;
+    esac
+done
 
--- ansible/roles/setup-pistream/files/menu.sh
+++ ansible/roles/setup-pistream/files/menu.sh
@@ -2,41 +2,101 @@
 # Felix Nesbigall - 01.02.2025
 # Displays streaming menu in openbox using rofi
 
-while true; do
+# Settings
 
-    CHOICE=$(echo -e "Movie Library\nStream Desktop\nBrowser\nSettings\nShutdown" | rofi -dmenu -p "PiStream" -i)
+LOGFILE=~/pistream-menu.log
+
+# Functions
+
+log_message() {
+    local message=$1
+    local line=$2
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $0 - INFO: in ${FUNCNAME[1]} - Line: $line - $message" | tee -a $LOGFILE | systemd-cat -t pistream
+}
+
+log_error() {
+    local message=${1:-"An error occurred."}
+    local line=$2
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $0 - ERROR: in ${FUNCNAME[1]} - Line: $line - CMD: $BASH_COMMAND - $message" | tee -a $LOGFILE | systemd-cat -t pistream
+}
+
+export -f log_message
+export -f log_error
+
+# Trap errors
+
+trap 'log_error "" "$LINENO"; exit 1' ERR
+
+# Main
+
+log_message "######## Starting menu.sh ########" $LINENO
+log_message "Entering main loop" $LINENO
+c=0
+while true; do
+    c=$((c+1))
+    log_message "Main loop iteration $c" $LINENO
+    CHOICE=$(echo -e "Movie Library\nDesktop Streaming\nNetflix\nDisney Plus\nAmazon Prime\nBrowser\nSettings\nShutdown" | rofi -dmenu -p "PiStream" -i)
 
     case "$CHOICE" in
         "Movie Library")
-            jellyfin
+            log_message "Movie Library selected" $LINENO
+            ERROR_OUTPUT=$(flatpak run com.github.iwalton3.jellyfin-media-player --fullscreen --tv) || log_error "Failed to execute jellyfin: $ERROR_OUTPUT" $LINENO
             ;;
-        "Stream Desktop")
-            moonlight-qt
+        "Desktop Streaming")
+            log_message "Desktop Streaming selected" $LINENO
+            ERROR_OUTPUT=$(moonlight-qt) || log_error "Failed to execute moonlight-qt: $ERROR_OUTPUT" $LINENO
+            ;;
+        "Netflix")
+            log_message "Netflix selected" $LINENO
+            ERROR_OUTPUT=$(chromium-browser --app=https://www.netflix.com --kiosk) || log_error "Failed to execute chromium-browser --app=https://www.netflix.com --kiosk: $ERROR_OUTPUT" $LINENO
+            ;;
+        "Disney Plus")
+            log_message "Disney Plus selected" $LINENO
+            ERROR_OUTPUT=$(chromium-browser --app=https://www.disneyplus.com --kiosk) || log_error "Failed to execute chromium-browser --app=https://www.disneyplus.com --kiosk: $ERROR_OUTPUT" $LINENO
+            ;;
+        "Amazon Prime")
+            log_message "Amazon Prime selected" $LINENO
+            ERROR_OUTPUT=$(chromium-browser --app=https://www.amazon.de/gp/video/storefront --kiosk) || log_error "Failed to execute chromium-browser --app=https://www.amazon.de/gp/video/storefront --kiosk: $ERROR_OUTPUT" $LINENO
             ;;
         "Browser")
-            chromium-browser -start-maximized
+            log_message "Browser selected" $LINENO
+            ERROR_OUTPUT=$(chromium-browser -start-maximized) || log_error "Failed to execute chromium-browser -start-maximized: $ERROR_OUTPUT" $LINENO
             ;;
         "Settings")
-            SETTINGS_CHOICE=$(echo -e "Bluetooth\nNetwork\nShow Devices\nShell\nBack" | rofi -dmenu -p "Settings" -i)
-            case "$SETTINGS_CHOICE" in
-                "Bluetooth")
-                    . ./bluetooth-settings.sh
-                    ;;
-                "Network")
-                    . ./network-settings.sh
-                    ;;
-                "Show Devices")
-                    . ./show-devices.sh
-                    ;;
-                "Shell")
-                    lxterminal
-                    ;;
-                "Back")
-                    ;;
-            esac
+            log_message "Settings selected" $LINENO
+            log_message "Entering settings loop" $LINENO
+            d=0
+            while true; do
+                d=$((d+1))
+                log_message "Settings loop iteration $d" $LINENO
+                SETTINGS_CHOICE=$(echo -e "Bluetooth\nNetwork\nShow Devices\nShell\nBack" | rofi -dmenu -p "Settings" -i)
+                case "$SETTINGS_CHOICE" in
+                    "Bluetooth")
+                        log_message "Bluetooth selected" $LINENO
+                        ERROR_OUTPUT=$(. ./bluetooth-settings.sh) || log_error "Failed to execute bluetooth-settings.sh: $ERROR_OUTPUT" $LINENO
+                        ;;
+                    "Network")
+                        log_message "Network selected" $LINENO
+                        ERROR_OUTPUT=$(. ./network-settings.sh) || log_error "Failed to execute network-settings.sh: $ERROR_OUTPUT" $LINENO
+                        ;;
+                    "Show Devices")
+                        log_message "Show Devices selected" $LINENO
+                        ERROR_OUTPUT=$(. ./show-devices.sh) || log_error "Failed to execute show-devices.sh: $ERROR_OUTPUT" $LINENO
+                        ;;
+                    "Shell")
+                        log_message "Shell selected" $LINENO
+                        ERROR_OUTPUT=$(lxterminal) || log_error "Failed to execute lxterminal: $ERROR_OUTPUT" $LINENO
+                        ;;
+                    "Back")
+                        log_message "Back selected" $LINENO
+                        break
+                        ;;
+                esac
+            done
             ;;
         "Shutdown")
-            echo "Shutdown"
+            log_message "Shutdown selected" $LINENO
+            ERROR_OUTPUT=$(sudo shutdown -h now) || log_error "Failed to execute sudo shutdown -h now: $ERROR_OUTPUT" $LINENO
             ;;
     esac
 done    
